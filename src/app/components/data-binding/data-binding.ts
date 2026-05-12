167477d998f0824ba44dcf3ff9786e790b8e5f03
Adriano Macedo <adriano.dsmacedo@outlook.com>
2026-05-12 11:12:59 -0300

    Ligação de dados em angular

---
+++ src/app/components/data-binding/data-binding.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  
+  courseName: string = "Angular 20 Tutorial";
+  
+  productPrice : number = 12500;
+
+ maxlength : number = 5;
+  
+ minChar: number = 3;
+
+ constructor()  {
+  }
+}
