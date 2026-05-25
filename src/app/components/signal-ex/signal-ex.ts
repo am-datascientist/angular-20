0225341130a454e82a76abd866d5ab61388a3806
Adriano Macedo <adriano.dsmacedo@outlook.com>
2026-05-25 10:55:45 -0300

    Signal em angular

---
+++ src/app/components/signal-ex/signal-ex.ts
@@ -0,0 +1,30 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-ex',
+  imports: [],
+  templateUrl: './signal-ex.html',
+  styleUrl: './signal-ex.css',
+})
+export class SignalEx {
+
+  firstName: string = "Adriano";
+  productName = "Moto"
+
+  courseName = signal<string>("Angular");
+
+  courseDuration = signal("15 videos");
+
+  courseDetail = computed(() => this.courseName() + " - " + this.courseDuration())
+
+  constructor() {
+    this.firstName = "Rahul";
+    console.log(this.firstName)
+    console.log(this.courseName())
+    setTimeout(() => {
+    this.courseName.set("React")
+    }, 5000);
+    
+    console.log(this.courseName())
+  }
+}
